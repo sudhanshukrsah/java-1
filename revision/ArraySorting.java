96451bc9122303cc4c29b38e588917fadb0895ff
sudhanshu kumar <sudhanshu_24a12res696@iitp.ac.in>
2025-09-15 19:47:06 +0530

    Add Java programs for Armstrong number check, array sorting, vowel counting, prime number check, and sum of digits

---
+++ revision/ArraySorting.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class ArraySorting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+        // Bubble sort
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.print("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ revision/CountVowels.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class CountVowels {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine().toLowerCase();
+        sc.close();
+        int count = 0;
+        for (char c : input.toCharArray()) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+        }
+        System.out.println("Number of vowels: " + count);
+    }
+}
